f641a694f5ef544f652615ffea3ff14e65332b12
mschaefer <mschaefer@DESKTOP-T7G4KHS>
2017-09-28 16:04:19 +0200

    Ready for home office. Getting the event streams done

--- HelloRS/MainWindow.xaml.cs
+++ HelloRS/MainWindow.xaml.cs
@@ -10,25 +10,61 @@ namespace HelloRS
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
+   
+
     public partial class MainWindow : Window
     {
+        PXCMSession session;
+        PXCMSenseManager.Handler handler;
 
         public MainWindow()
         {
             InitializeComponent();
 
+            // Setupt Eventhandler 
+            handler = new PXCMSenseManager.Handler();
+            
+            // Get instance of SenseManager 
+            session = PXCMSession.CreateInstance();
+
         }
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
+            session.Dispose();
 
         }
-
-
-        private void Button_Click(object sender, RoutedEventArgs e)
+        
+        pxcmStatus OnNewSample(int mid, PXCMCapture.Sample sample)
         {
-            // Get instance of SenseManager 
-            PXCMSession session = PXCMSession.CreateInstance();
+            PXCMImage image = sample.color;
+            PXCMImage.ImageData data;
+            image.AcquireAccess(PXCMImage.Access.ACCESS_READ, PXCMImage.PixelFormat.PIXEL_FORMAT_RGB32, out data);
+            WriteableBitmap wbm = data.ToWritableBitmap(0,
+                                      image.info.width,
+                                     image.info.height,
+                                           96.0, 96.0);
+            wbm.Freeze();
+            imageRGB.Dispatcher.Invoke(
+                 new Action(() => imageRGB.Source = wbm));
+            GC.Collect();
+            image.ReleaseAccess(data);
+            return pxcmStatus.PXCM_STATUS_NO_ERROR;
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            btnStart.IsEnabled = true;
+            btnStop.IsEnabled = false;        
+            session.Dispose();
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            btnStart.IsEnabled = false;
+            btnStop.IsEnabled = true;
 
             // Get RS version 
             PXCMSession.ImplVersion version = session.QueryVersion();
@@ -39,47 +75,29 @@ namespace HelloRS
 
             // Get streams ready 
             sm.EnableStream(PXCMCapture.StreamType.STREAM_TYPE_COLOR, 640, 480);
-            sm.EnableStream(PXCMCapture.StreamType.STREAM_TYPE_DEPTH, 640, 480);
+
+            // Create Eventhandler             
+            handler.onNewSample = OnNewSample;
 
             // Init Pipeline 
-            sm.Init();
+            sm.Init(handler);
+            sm.StreamFrames(false);
+
+
 
             // Get samples
             pxcmStatus status = sm.AcquireFrame(true); // Synchronous capturing 
             PXCMCapture.Sample sample = sm.QuerySample();
 
-            // Convert samples to image 
-            PXCMImage image = sample.color;
-            PXCMImage dimage = sample.depth;
 
-            PXCMImage.ImageData data;
-            image.AcquireAccess(PXCMImage.Access.ACCESS_READ, PXCMImage.PixelFormat.PIXEL_FORMAT_RGB32, out data);
-            WriteableBitmap wbm = data.ToWritableBitmap(0,
-                                  image.info.width,
-                                  image.info.height,
-                                  96.0, 96.0);
-
-            PXCMImage.ImageData data2;
-            dimage.AcquireAccess(PXCMImage.Access.ACCESS_READ, PXCMImage.PixelFormat.PIXEL_FORMAT_DEPTH_RAW, out data2);
-            WriteableBitmap wbm2 = data2.ToWritableBitmap(
-                                   0,
-                                   dimage.info.width,
-                                   dimage.info.height,
-                                   96.0, 96.0);
-
-            // Display image 
-            imageRGB.Source = wbm;
-            imageDepth.Source = wbm2;
-
-
-            // Clean up 
-            image.ReleaseAccess(data);
-            image.ReleaseAccess(data2);
+            
             sm.ReleaseFrame();
 
 
             sm.Close();
-            session.Dispose();
+            
+
+            
         }
     }
 }
